85515d1b1de056432be67cb5ee1c7c92c4cb6993
Aram Dulyan <aram00@gmail.com>
2012-01-28 07:53:17 +1100

    Fleshed out thread implementation and painting the canvas blue.

--- src/com/aramgutang/games/snakesaver/SnakesaverThread.java
+++ src/com/aramgutang/games/snakesaver/SnakesaverThread.java
@@ -1,5 +1,38 @@
 package com.aramgutang.games.snakesaver;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.view.SurfaceHolder;
+
 public class SnakesaverThread extends Thread {
+	public Boolean running = false;
 	
+	public SurfaceHolder surface_holder;
+	public Context context;
+	public Handler handler;
+	
+	public SnakesaverThread(SurfaceHolder surface_holder, Context context, Handler handler) {
+		this.surface_holder = surface_holder;
+		this.context = context;
+		this.handler = handler;
+	}
+	
+    @Override
+    public void run() {
+        while(this.running) {
+            Canvas canvas = null;
+            try {
+                canvas = this.surface_holder.lockCanvas(null);
+                this.draw(canvas);
+            } finally {
+                if (canvas != null)
+                    this.surface_holder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+    
+    private void draw(Canvas canvas) {
+    	canvas.drawRGB(76, 202, 237);
+    }
 }
--- src/com/aramgutang/games/snakesaver/SnakesaverView.java
+++ src/com/aramgutang/games/snakesaver/SnakesaverView.java
@@ -1,6 +1,7 @@
 package com.aramgutang.games.snakesaver;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -11,22 +12,44 @@ public class SnakesaverView extends SurfaceView implements SurfaceHolder.Callbac
 	public SnakesaverView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.setSystemUiVisibility(SYSTEM_UI_FLAG_LOW_PROFILE);
-	}
-
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
 		
+		SurfaceHolder holder = this.getHolder();
+		holder.addCallback(this);
+		
+		this.thread = new SnakesaverThread(holder, context, new Handler());
+		
+		this.setFocusable(true);
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
-		
+		this.thread.running = true;
+		this.thread.start();
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
-		
+        // We have to tell thread to shut down & wait for it to finish, or else
+        // it might touch the Surface after we return and explode
+        boolean retry = true;
+        thread.running = false;
+        while (retry) {
+            try {
+                thread.join();
+                retry = false;
+            } catch (InterruptedException e) {}
+        }
 	}
-
+	
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+//        if (!hasWindowFocus)
+//        	thread.pause();
+    }
+	
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		//thread.setSurfaceSize(width, height);
+	}
 }
