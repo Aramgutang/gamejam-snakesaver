3898ef01b335cc4c09d6cd211860e4157308747c
Aram Dulyan <aram00@gmail.com>
2012-01-28 04:37:58 +1100

    Initial commit: it runs, and is landscape only.

---
+++ src/com/aramgutang/games/snakesaver/SnakesaverActivity.java
@@ -0,0 +1,12 @@
+package com.aramgutang.games.snakesaver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SnakesaverActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
